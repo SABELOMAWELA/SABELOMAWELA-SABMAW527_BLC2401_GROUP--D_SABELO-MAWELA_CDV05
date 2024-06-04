e6648859944478ec107fc4482abc33434de7a29d
SABELO MAWELA <MAWELAS63@GMAIL.COM>
2024-06-04 13:51:26 +0200

    "Added loader CSS animation and updated form ID in index.html, also added script tag for index.js"

---
+++ index.js
@@ -0,0 +1,6 @@
+
+ document.querySelector("#hireMe").addEventListener("submit", (event) => {
+    
+    event.target.innerHTML = `<span>Please wait this might take a few minutes</span> <div class="loader"></div> `
+    ;
+})
