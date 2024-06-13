f7c55a348dc49819ed348345beca0cc596145bb4
SABELO MAWELA <MAWELAS63@GMAIL.COM>
2024-06-13 20:11:02 +0200

    "Updated CSS and HTML for navigation and layout, added hamburger menu and responsive design changes, and modified JavaScript for navigation toggle functionality."

--- index.js
+++ index.js
@@ -4,4 +4,14 @@
     event.target.innerHTML = `<span>Please wait this might take a few minutes</span> <div class="loader"></div> `
     ;
 })
+document.addEventListener('DOMContentLoaded', function() {
+  const hamburger = document.querySelector('.hamburger');
+  const navLinks = document.querySelectorAll('.navlist');
+
+  hamburger.addEventListener('click', function() {
+    navLinks.forEach(link => {
+      link.classList.toggle('active');
+    });
+  });
+});
 
